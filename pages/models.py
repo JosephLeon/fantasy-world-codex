1caa2b29f3e9f9c59517ffa28ecd64f48693743b
Joseph Leon <joseph@poeticsystems.com>
2014-11-13 16:43:11 +0200

    Fix CharField migration error.

--- pages/models.py
+++ pages/models.py
@@ -2,28 +2,28 @@ from django.db import models
 
 
 class Region(models.Model):
-    region_name = models.Charfield(max_length=200)
+    region_name = models.CharField(max_length=200)
 
 
 class Place(models.Model):
     region = models.ForeignKey(Region)
-    place_name = models.Charfield(max_length=200)
+    place_name = models.CharField(max_length=200)
 
 
 class Building(models.Model):
     region = models.ForeignKey(Region)
     place = models.ForeignKey(Place)
-    building_name = models.Charfield(max_length=200)
+    building_name = models.CharField(max_length=200)
 
 
 class Character(models.Model):
     place = models.ForeignKey(Place)
     building = models.ForeignKey(Building)
-    character_name = models.Charfield(max_length=200)
+    character_name = models.CharField(max_length=200)
 
 
 class Item(models.Model):
     place = models.ForeignKey(Place)
     building = models.ForeignKey(Building)
     character = models.ForeignKey(Character)
-    item_name = models.Charfield(max_length=200)
+    item_name = models.CharField(max_length=200)
