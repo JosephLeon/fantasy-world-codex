b35af50572af8369bafa8659e4f5ab78dd79df5b
Joseph Leon <joseph@poeticsystems.com>
2014-11-13 16:37:09 +0200

    Add model structure to pages.

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Region(models.Model):
+    region_name = models.Charfield(max_length=200)
+
+
+class Place(models.Model):
+    region = models.ForeignKey(Region)
+    place_name = models.Charfield(max_length=200)
+
+
+class Building(models.Model):
+    region = models.ForeignKey(Region)
+    place = models.ForeignKey(Place)
+    building_name = models.Charfield(max_length=200)
+
+
+class Character(models.Model):
+    place = models.ForeignKey(Place)
+    building = models.ForeignKey(Building)
+    character_name = models.Charfield(max_length=200)
+
+
+class Item(models.Model):
+    place = models.ForeignKey(Place)
+    building = models.ForeignKey(Building)
+    character = models.ForeignKey(Character)
+    item_name = models.Charfield(max_length=200)
