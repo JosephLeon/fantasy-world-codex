9a8ea5803e845c723a9fc17b5b037f3796bb43a2
Joseph Leon <joseph@poeticsystems.com>
2014-11-16 00:50:50 +0200

    Add pattern that references pages urls.

--- fantasyworldcodex/urls.py
+++ fantasyworldcodex/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'fantasyworldcodex.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^pages/', include('pages.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
