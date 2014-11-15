170c248b8e89c4c4de5528f2bc832f3ac877b5ae
Joseph Leon <joseph@poeticsystems.com>
2014-11-15 23:57:24 +0200

    Add classes to admin.

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from pages.models import Region, Place, Building, Character, Item
 
-# Register your models here.
+admin.site.register(Region)
+admin.site.register(Place)
+admin.site.register(Building)
+admin.site.register(Character)
+admin.site.register(Item)
