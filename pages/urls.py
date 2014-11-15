56ee0aec9b435d04c46048db2d7519f055d3b028
Joseph Leon <joseph@poeticsystems.com>
2014-11-16 01:14:21 +0200

    Add url patterns and httpresponses.

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,10 @@ from pages import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
+    # ex: /pages/5/
+    url(r'^(?P<character_id>\d+)/$', views.detail, name='detail'),
+    # ex: /pages/5/results/
+    url(r'^(?P<question_id>\d+)/results/$', views.results, name='results'),
+    # # ex: /pages/5/region/
+    # url(r'^(?P<question_id>\d+)/region/$', views.vote, name='region'),
 )
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,16 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('Welcome to Fantasy World Codex!')
+
+
+def detail(request, character_id):
+    return HttpResponse("Behold! this is the page of %s" % character_id)
+
+
+def results(request, character_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % character_id)
+
+
+# def region(request, region_id):
+#     return HttpResponse("The region is %s." % region_id)
