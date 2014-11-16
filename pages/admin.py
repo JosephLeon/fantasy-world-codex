77c6e788d32de30a244722d9a82ec4979b51028d
Joseph Leon <joseph@poeticsystems.com>
2014-11-16 20:41:57 +0200

    Add building to admin.

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from pages.models import Region, Place, Character, Item
+from pages.models import Region, Place, Building, Character, Item
 
 admin.site.register(Region)
 admin.site.register(Place)
+admin.site.register(Building)
 admin.site.register(Character)
 admin.site.register(Item)
