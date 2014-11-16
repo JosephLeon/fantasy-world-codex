22d741b117fc1c3d3db24f311fd9472d155173f4
Joseph Leon <joseph@poeticsystems.com>
2014-11-16 12:18:32 +0200

    Convert functions to render shortcut.

--- pages/views.py
+++ pages/views.py
@@ -1,27 +1,20 @@
-from django.http import HttpResponse
-from django.template import RequestContext, loader
+from django.shortcuts import render
 from pages.models import Region
 
 
 def index(request):
     region_list = Region.objects.all()
-    template = loader.get_template('pages/index.html')
-    context = RequestContext(request, {
-        'region_list': region_list,
-    })
-    return HttpResponse(template.render(context))
-    # output = ', '.join([p.region_name for p in region_list])
-    # return HttpResponse(output)
+    context = {'region_list': region_list}
+    return render(request, 'pages/index.html', context)
 
 
 def detail(request, character_id):
-    return HttpResponse("Behold! this is the page of %s" % character_id)
+    return render(request, 'pages/index.html')
 
 
 def results(request, character_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % character_id)
+    return render(request, 'pages/index.html')
 
 
 def region(request, region_id):
-    return HttpResponse("The region is %s." % region_id)
+    return render(request, 'pages/index.html')
