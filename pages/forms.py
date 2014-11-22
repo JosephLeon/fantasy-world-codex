b477d286d8b2fec5d61226718f19faa9cc615d75
Joseph Leon <joseph@poeticsystems.com>
2014-11-23 01:36:02 +0200

    Add form for add page.

--- pages/forms.py
+++ pages/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-from pages.models import Region
+from pages.models import Region, Place
 
 
 class RegionForm(forms.ModelForm):
@@ -11,6 +11,18 @@ class RegionForm(forms.ModelForm):
         model = Region
         fields = ('name',)
         exclude = ('description',)
+
+
+class PlaceForm(forms.ModelForm):
+    name = forms.CharField(max_length=200, help_text="Name:")
+    population = forms.IntegerField(initial=0, help_text='Population:')
+    description = forms.CharField(widget=forms.Textarea, help_text='Description:')
+    economy = forms.CharField(widget=forms.Textarea, help_text='Economy:')
+    special_features = forms.CharField(widget=forms.Textarea, help_text='Special Features:')
+
+    class Meta:
+        model = Place
+        fields = ('name', 'population', 'description', 'economy', 'special_features')
 
 
 class UserForm(forms.ModelForm):
