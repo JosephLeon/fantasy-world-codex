d43c60b5ef2d1284b458eb3305c03936c3751e15
Joseph Leon <joseph@poeticsystems.com>
2014-11-22 18:33:40 +0200

    Add view function register and the url mapping for the register function.

--- pages/urls.py
+++ pages/urls.py
@@ -4,9 +4,7 @@ from pages import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    # ex: /pages/regions/5/
-    # url(r'^regions/(?P<region_id>\d+)/$', views.detail, name='detail'),
     url(r'^regions/(?P<region_id>\d+)/$', views.region, name='region'),
-    # /pages/places/5/
     url(r'^places/(?P<place_id>\d+)/$', views.place, name='place'),
+    url(r'^register/$', views.register, name='register'),
 )
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from pages.models import Region, Place
+# from django.contrib.auth.models import User
+from pages.forms import UserForm
 
 
 def index(request):
@@ -21,3 +23,26 @@ def region(request, region_id):
 def place(request, place_id):
     place = get_object_or_404(Place, pk=place_id)
     return render(request, 'pages/place.html', {'place': place})
+
+
+def register(request):
+    registered = False
+
+    if request.method == 'POST':
+        user_form = UserForm(data=request.POST)
+        if user_form.is_valid:
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            # profile.save()
+            registered = True
+        else:
+            print user_form.errors
+    else:
+        user_form = UserForm()
+
+    return render(
+        request,
+        'pages/registration.html',
+        {'user_form': user_form, 'registered': registered}
+    )
