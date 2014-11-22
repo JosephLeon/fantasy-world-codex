83008640c14aa61997d95db93b5737553071e207
Joseph Leon <joseph@poeticsystems.com>
2014-11-23 01:37:02 +0200

    Add url mapping for add page.

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,7 @@ from pages import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
     url(r'^add_region/$', views.add_region, name='add_region'),
+    url(r'^add_place/$', views.add_place, name='add_place'),
     url(r'^regions/(?P<region_id>\d+)/$', views.region, name='region'),
     url(r'^places/(?P<place_id>\d+)/$', views.place, name='place'),
     url(r'^register/$', views.register, name='register'),
