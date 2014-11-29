ff145f4fb1a7563016567862a4f0cc1d4457e372
Joseph Leon <joseph@poeticsystems.com>
2014-11-29 22:18:47 +0200

    Remove commented code.

--- pages/views.py
+++ pages/views.py
@@ -8,12 +8,6 @@ def index(request):
     region_list = Region.objects.all()
     context = {'region_list': region_list}
     return render(request, 'pages/index.html', context)
-    # return render(request, 'Holder')
-
-
-# def detail(request, region_id):
-#     region = get_object_or_404(Region, pk=region_id)
-#     return render(request, 'pages/detail.html', {'region': region})
 
 
 def region(request, region_id):
