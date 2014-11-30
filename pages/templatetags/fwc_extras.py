39c331f4fbdee1b0d1108b5f0b7604a1650d9c2b
Joseph Leon <joseph@poeticsystems.com>
2014-11-30 18:27:03 +0200

    Added a get other related buildings with filter logic, added that template tag to the base template

--- pages/templatetags/fwc_extras.py
+++ pages/templatetags/fwc_extras.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django import template
-from pages.models import Place, Region
+from pages.models import Place, Region, Building, Character
 
 register = template.Library()
 
@@ -17,5 +17,16 @@ def get_other_places_list(place_id):
     places = Place.objects.all()
     context_dict['places'] = places
     this_object = get_object_or_404(Place, pk=place_id)
+    context_dict['this_object'] = this_object
+    return context_dict
+
+
+@register.inclusion_tag('pages/other_buildings.html')
+def get_other_buildings_list(building_id):
+    context_dict = {}
+
+    buildings = Building.objects.all()
+    context_dict['buildings'] = buildings
+    this_object = get_object_or_404(Building, pk=building_id)
     context_dict['this_object'] = this_object
     return context_dict
