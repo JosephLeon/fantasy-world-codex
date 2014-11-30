02fe6cb693f8f6235e49bfd3031818435dd68cb9
Joseph Leon <joseph@poeticsystems.com>
2014-11-30 04:05:19 +0200

    Add argument for place id.

--- pages/templatetags/fwc_extras.py
+++ pages/templatetags/fwc_extras.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from django import template
 from pages.models import Place, Region
 
@@ -10,5 +11,11 @@ def get_region_list():
 
 
 @register.inclusion_tag('pages/other_places.html')
-def get_other_places_list():
-    return {'places': Place.objects.all()}
+def get_other_places_list(place_id):
+    context_dict = {}
+
+    places = Place.objects.all()
+    context_dict['places'] = places
+    this_object = get_object_or_404(Place, pk=place_id)
+    context_dict['this_object'] = this_object
+    return context_dict
