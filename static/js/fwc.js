960afb5a118ed68b3f23b319ce19ad93f1d97cda
Joseph Leon <joseph@poeticsystems.com>
2014-12-04 03:56:47 +0200

    Testing and cleaning code.

--- static/js/fwc.js
+++ static/js/fwc.js
@@ -2,11 +2,12 @@
   // alert('test');
   console.log('TESTING');
 
-  $(document).ready(function() {
-      function newParameters(query) {
-          query.state = $('#id_region').val();
-      }
-      $('#id_place_0').djselectable('option', 'prepareQuery', newParameters);
-  });
+  console.log($('#id_region').val());
+  function newParameters(query) {
+      query.state = $('#id_region').val();
+      console.log(query.state);
+      console.log('Inside.');
+  }
+  $('#id_place_0').djselectable('option', 'prepareQuery', newParameters);
 
 })(jQuery, this, this.document);
