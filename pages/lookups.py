fbd31493d7f54ec08d69324291f746445dedd6b8
Joseph Leon <joseph@poeticsystems.com>
2014-12-05 01:22:26 +0200

    Add lookup function for building.

--- pages/lookups.py
+++ pages/lookups.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from selectable.base import ModelLookup
 from selectable.registry import registry
 
-from pages.models import Place
+from pages.models import Place, Building
 
 
 class PlaceLookup(ModelLookup):
@@ -24,3 +24,23 @@ class PlaceLookup(ModelLookup):
 
 
 registry.register(PlaceLookup)
+
+
+class BuildingLookup(ModelLookup):
+    model = Building
+    search_fields = ('name__icontains', )
+
+    def get_query(self, request, term):
+        results = super(BuildingLookup, self).get_query(request, term)
+        print results
+        place = request.GET.get('place', '')
+        print place
+        if place:
+            results = results.filter(place=place)
+        return results
+
+    def get_item_label(self, item):
+        return "%s, %s" % (item.name, item.place)
+
+
+registry.register(BuildingLookup)
