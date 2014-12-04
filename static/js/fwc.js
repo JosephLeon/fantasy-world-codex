ae4edfc629897feca9d2c9e0cf92a228cb101e01
Joseph Leon <joseph@poeticsystems.com>
2014-12-05 01:22:54 +0200

    Add javascript functionality to update building select with filters from place.

--- static/js/fwc.js
+++ static/js/fwc.js
@@ -10,4 +10,14 @@
     $('#id_place_0').djselectable('option', 'prepareQuery', newParameters);
   });
 
+  $('#id_place_0').on('change', function() {
+    $('#id_building_0 option:eq(0)').prop("selected", true);
+    $('#id_building_0').val('Select a building');
+    function newParameters(query) {
+      query.region = $('#id_place_0').val();
+    }
+
+    $('#id_building_0').djselectable('option', 'prepareQuery', newParameters);
+  });
+
 })(jQuery, this, this.document);
