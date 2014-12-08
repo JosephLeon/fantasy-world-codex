974c5293926486ef16b57e7355cd49320135669d
Joseph Leon <joseph@poeticsystems.com>
2014-12-08 03:09:02 +0200

    Change code to hardcode options.

--- static/js/fwc.js
+++ static/js/fwc.js
@@ -1,5 +1,39 @@
 (function ($, window, document, undefined) {
 
+  // Get option values from feeds_subcat view.
+  $("select#id_topregion").change(function(){
+    // Tells if we make it inside change function.
+    console.log('Inside $("select#id_topregion").change(function().');
+    $.getJSON("/pages/feeds/places/"+$(this).val()+"/", function(j) {
+      // Json objects.
+      console.log(j);
+      // Json objects count.
+      console.log(j.length);
+      var items = [];
+      var options = '<option value="">Select place</option>';
+      // $.each(j, function(j.fields['region'], j.fields['name'])) {
+      //   items.push('<option value="' + j.fields['region'] + '">' + j.fields['name'] + '</options>');
+      // }
+      // $.each( data, function( key, val ) {
+      //   items.push( "<li id='" + key + "'>" + val + "</li>" );
+      // });
+
+      // $( "<ul/>", {
+      //   "class": "my-new-list",
+      //   html: items.join( "" )
+      // }).appendTo( "body" );
+      for (var i = 0; i < j.length; i++) {
+        // console.log(j.fields.name);
+        options += '<option value="primary key goes here">place name here</option>';
+        // options += '<option value="' + j.pk + '">' + j.fields['name'] + '</option>';
+      }
+      $("#id_subplace").html(options);
+      $("#id_subplace option:first").attr('selected', 'selected');
+      $("#id_subplace").attr('disabled', false);
+    });
+    $("#id_topregion").attr('selected', 'selected');
+  });
+
   $('#id_place_0').attr('placeholder', 'Select a place');
   $('#id_building_0').attr('placeholder', 'Select a building');
 
@@ -16,19 +50,6 @@
     }
 
     $('#id_place_0').djselectable('option', 'prepareQuery', newParameters);
-  });
-
-  $("select#id_topregion").change(function(){
-    $.getJSON("/pages/feeds/places/"+$(this).val()+"/", function(j) {
-      var options = '<option value="">---------- </option>';
-      for (var i = 0; i < j.length; i++) {
-        options += '<option value="' + parseInt(j[i].pk) + '">' + j[i].fields['longname'] + '</option>';
-      }
-      $("#id_subplace").html(options);
-      $("#id_subplace option:first").attr('selected', 'selected');
-      $("#id_subplace").attr('disabled', false);
-    });
-    $("#id_topregion").attr('selected', 'selected');
   });
 
   // $( "#id_place_0" ).blur(function() {
