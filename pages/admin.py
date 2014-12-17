d8ef59244c419c73a2fd7f8331fe5d506bbf5679
Joseph Leon <joseph@poeticsystems.com>
2014-12-17 19:35:52 +0200

    Add location to admin panel.

--- pages/admin.py
+++ pages/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from pages.models import Region, Place, Building, Character, Item, Beast
+from pages.models import Region, Place, Building, Character, Item, Beast, Location
 
 admin.site.register(Region)
 admin.site.register(Place)
@@ -7,3 +7,4 @@ admin.site.register(Building)
 admin.site.register(Character)
 admin.site.register(Item)
 admin.site.register(Beast)
+admin.site.register(Location)
